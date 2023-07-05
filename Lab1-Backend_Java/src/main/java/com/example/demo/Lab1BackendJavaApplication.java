ce0798806cad23afffdd676c747201ec488e49f7
tongkungtk <theeranan48@gmail.com>
2023-07-05 11:34:19 +0700

    Finish Projec Lab1 Add floder

---
+++ Lab1-Backend_Java/src/main/java/com/example/demo/Lab1BackendJavaApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1BackendJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab1BackendJavaApplication.class, args);
+		System.out.println("Hello");
+	}
+
+}
