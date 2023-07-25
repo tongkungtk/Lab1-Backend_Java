0b2382ee8beb5ae78971fda725132a38ab3d3e9c
tongkungtk <theeranan48@gmail.com>
2023-07-26 01:42:00 +0700

    Finish Lab2 19/07/66

--- Backend_Java/src/main/java/com/example/demo/controller/Employeecontroller.java
+++ Backend_Java/src/main/java/com/example/demo/controller/Employeecontroller.java
@@ -3,9 +3,11 @@ package com.example.demo.controller;
 import java.util.ArrayList;
 import java.util.List;
 
-
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,22 +17,69 @@ import com.example.demo.model.Employee;
 public class EmployeeController {
 
 	private List<Employee> data = new ArrayList<Employee>();
-	
+
 	@GetMapping("/employee")
-	public List<Employee> getEmployees(){
+	public List<Employee> getEmployees() {
 		return data;
 	}
-	
+
 	@PostMapping("/employee")
 	public Employee addEmployee(@RequestBody Employee body) {
-		
+
 		for (int i = 0; i < data.size(); i++) {
-		    if (data.get(i).getEmployeeId() == body.getEmployeeId()) {
-		        return null;
-		    }
+			if (data.get(i).getEmployeeId() == body.getEmployeeId()) {
+				return null;
+			}
 		}
 
 		data.add(body);
 		return body;
 	}
-}
+
+	@GetMapping("/employee/{employeeId}")
+
+	public Employee getEmployee(@PathVariable Integer employeeId) {
+		System.out.println("employeeId = " + employeeId);
+
+		for (int i = 0; i < data.size(); i++) {
+			if (employeeId == data.get(i).getEmployeeId()) {
+				return data.get(i);
+			}
+		}
+
+		return null;
+	}
+
+	@PutMapping("employee/{employeeId}")
+	public Employee updateEmployee(@PathVariable Integer employeeId, @RequestBody Employee body)  {
+		for (int i = 0; i < data.size(); i++) {
+			if (employeeId == data.get(i).getEmployeeId()) {
+				data.get(i).setFirstName(body.getFirstName());
+				data.get(i).setLastName(body.getLastName());
+				data.get(i).setSalary(body.getSalary());
+				
+				return data.get(i); 
+			}
+		}
+		return null;
+	}
+	
+	@DeleteMapping("employee/{employeeId}")
+	public String deleteemploye(@PathVariable Integer employeeId) {
+		for (int i = 0; i < data.size(); i++) {
+			if (employeeId == data.get(i).getEmployeeId()) {
+				data.remove(i);
+				return "delate success";
+			}
+		}	
+		return "employee no found";	
+	}
+
+	
+	
+	
+	
+	
+
+	
+	}
